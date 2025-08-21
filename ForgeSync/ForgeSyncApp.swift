58b2d3dd575683adbb47678a3263ab6438425115
Rangerboy574 <jdeweese574@icloud.com>
2025-08-21 14:22:25 -0500

    Initial Commit

---
+++ ForgeSync/ForgeSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgeSyncApp.swift
+//  ForgeSync
+//
+//  Created by Joseph DeWeese on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ForgeSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForgeSyncTests/ForgeSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForgeSyncTests.swift
+//  ForgeSyncTests
+//
+//  Created by Joseph DeWeese on 8/21/25.
+//
+
+import Testing
+@testable import ForgeSync
+
+struct ForgeSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
